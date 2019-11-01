32218f62b99429ab61da35c4b915d39d1e8d9c38
MK <kaliszmat1@gmail.com>
2019-11-01 19:11:08 +0100

    introduction to nodejs

---
+++ START/app.js
@@ -0,0 +1,13 @@
+
+console.log(Object.getOwnPropertyNames(global).length);
+
+console.log(new Date());
+const hello = 'witaj node js!';
+
+console.log(hello);
+
+
+const os = require('os');
+console.log(os.totalmem());
+
+
---
+++ START/server/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((request,response) => {
+    console.log(request.url);
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('<h1>hello node!</h1>');
+})
+
+server.listen(5500, '127.0.0.1', ()=> console.log("serwer wystartowal"));
