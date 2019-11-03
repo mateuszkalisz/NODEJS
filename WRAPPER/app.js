423b8db7406ef0e48b69c35f8eb8f558e308874b
MK <kaliszmat1@gmail.com>
2019-11-03 21:07:30 +0100

    something about module, export and others - basics about nodejs

---
+++ WRAPPER/app.js
@@ -0,0 +1,8 @@
+const notes = require('./notes')
+console.log(notes);
+console.log("jestem w module app.js");
+
+const mod = require('module');
+
+console.log(mod.wrapper);
+
---
+++ WRAPPER/notes.js
@@ -0,0 +1,12 @@
+
+console.log("jestem w module notes.js");
+
+module.exports = "cos zwrocone z modulu notes";
+
+console.log('exports', exports);
+
+console.log('dirname', __dirname);
+
+console.log('filename', __filename);
+
+console.log('module', module);
