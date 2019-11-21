0bb93208512bf2cd3e9bf5731474814b3df62a51
MK <kaliszmat1@gmail.com>
2019-11-21 20:44:41 +0100

    routing on server by nodejs

--- FIRST_SERVER/code.js
+++
@@ -1,5 +0,0 @@
-const p = document.createElement("p");
-
-p.innerHTML = "<p>RAZDWA</p>";
-
-document.body.appendChild(p);
--- FIRST_SERVER/server.js
+++ FIRST_SERVER/server.js
@@ -1,9 +1,28 @@
 const http = require('http');
 
+const port = process.env.PORT || 3000;
+
 http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type': 'text/html;charset=utf-8'})
-    res.write("<h1>Serwer działa</h1>")
-    res.end(
-    // `<script src="./code.js"></script>`
-    )
-}).listen(3000, '127.0.0.1')
+    // console.log(req.url);
+    // console.log(req.method);
+
+    if(req.url === "/"){
+        res.writeHead(200,{"Content-type":"text/html;charset=utf-8"});
+        res.end("<h1>Strona główna</h1>");
+    }
+    else if(req.url ==="/users"){
+        res.writeHead(200,{"Content-type":"text/html;charset=utf-8"});
+        res.end("<p>Strona uzytkownika</p>");
+    }
+
+    else{
+        res.writeHead(404, {"Content-type":"text/html;charset=utf-8"});
+        res.end("<div>Brak takiej strony</div>");
+    }
+
+    // res.end(req.url);
+}).listen(port, '127.0.0.1', ()=>{
+    console.log(`nasz serwer nasluchuje na porcie ${port}`);
+})
+
+
