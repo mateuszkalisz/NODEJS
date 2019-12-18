3b1aaa2cf8a90e46e4e140f5b9e3fd912671aeec
MK <kaliszmat1@gmail.com>
2019-12-18 17:51:03 +0100

    create db and connect app.js with mongodb for todolist

--- EXPRESS/ToDoApp_express_mongo/app.js
+++ EXPRESS/ToDoApp_express_mongo/app.js
@@ -1,4 +1,29 @@
 const express = require('express');
+const mongo = require('mongodb');
+
+const client = new mongo.MongoClient('mongodb://localhost:27017', {useNewUrlParser: true});
+
+client.connect(err =>{
+    if(err){
+        console.log("błąd połączenia", err);
+    }
+    else{
+        console.log("połączenie udane!");
+
+        const db = client.db('test');
+        const todolist = db.collection('todolist');
+
+        todolist.find({}).toArray((err,todo)=>{
+            if(err){
+                console.log("bledne zapytanie");
+            }
+            else{
+                console.log(todo);
+            }
+        });
+        client.close();
+    }
+})
 
 const app = express();
 const port = process.env.PORT || 3000;
---
+++ EXPRESS/ToDoApp_express_mongo/public/script.js
@@ -0,0 +1,13 @@
+const showHide = document.querySelector("#button1");
+const removeDone = document.querySelector("#button2");
+const clearAll = document.querySelector("#button3");
+
+
+const input = document.querySelector("input");
+
+const addTask = document.querySelector("#button4");
+
+const tasksDone = document.querySelector("#done");
+
+const tasksToDo = document.querySelector("#todo");
+
