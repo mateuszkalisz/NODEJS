4e0caf7207cd8d3cff2eee2f3c1fa71879f03cfe
MK <kaliszmat1@gmail.com>
2020-01-16 21:33:15 +0100

    add basic styles and ai and user logic in frontend

--- EXPRESS/LOGOWANIE/public/game.js
+++ EXPRESS/LOGOWANIE/public/game.js
@@ -4,6 +4,8 @@ const wins = document.querySelector('#wins');
 const draws = document.querySelector('#draws');
 const losses = document.querySelector('#losses');
 const welcome = document.querySelector('#welcome');
+const playerChoices = document.querySelectorAll('.wrapper div');
+
 
 function loadSummary(){
 
@@ -12,7 +14,7 @@ function loadSummary(){
     })
     .then(res => res.json())
     .then(data => {
-        console.log(data);
+        // console.log(data);
         points.textContent = data.points;
         wins.textContent = data.wins;
         draws.textContent = data.draws;
@@ -35,3 +37,54 @@ aLogout.addEventListener('click', function(){
         {'Content-Type':'application/json'}
     })
 })
+
+
+function whoWin(playerChoice){
+
+    let possibilities = [
+        {
+            id: 1,
+            name: 'paper',
+            choice: 'first'
+        },
+        {
+            id: 2,
+            name: 'scissors',
+            choice: 'second'
+        },
+        {
+            id: 3,
+            name: 'stone',
+            choice: 'third'
+        }
+    ];
+
+    let randomChoice = [];
+
+    const index = Math.floor(Math.random()*(possibilities.length-1));
+    randomChoice.push(possibilities[index]);
+    possibilities.splice(index, 1);
+
+    const aiChoice = randomChoice[0].choice;
+
+    const userChoice = playerChoice.className;
+    
+    if(userChoice == aiChoice){
+        console.log("remis");
+    }
+    else if((userChoice == "first" && aiChoice == "third") || (userChoice == "second" && aiChoice == "first") || (userChoice == "third" && aiChoice == "second")){
+        console.log("wygrales");
+    }
+    else{
+        console.log("przegrales");
+    }
+}
+
+
+
+playerChoices.forEach(playerChoice => playerChoice.addEventListener('click', ()=>{
+    whoWin(playerChoice);
+}))
+
+
+
