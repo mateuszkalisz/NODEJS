687f56216e697c8d5344866703c3adf8a5d97ef1
mateuszkalisz <38287804+mateuszkalisz@users.noreply.github.com>
2020-02-08 16:10:50 +0100

    Add files via upload
    create api

---
+++ routes/quiz.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const Quiz = require('../models/quiz');
+
+/* GET home page. */
+router.get('/', (req, res) =>{
+
+  const show = !req.session.vote;
+
+  Quiz.find({}, (err,data)=>{
+
+    let sum = 0;
+
+    data.forEach(item => {
+      sum +=item.vote;
+    })
+
+    res.render('quiz', { title: 'Quiz', data, show, sum});
+  })
+
+});
+
+router.post('/', (req,res)=>{
+  // console.log(req.body.quiz);
+
+  const id = req.body.quiz;
+
+  Quiz.findOne({_id: id}, (error, quiz)=>{
+
+    quiz.vote = quiz.vote + 1;
+    quiz.save((err)=>{
+      req.session.vote = 1;
+      res.redirect('/quiz');
+    });
+  })
+})
+
+module.exports = router;
