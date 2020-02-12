b99f1f905357f5679d6b85e487e727bdf3dd1e51
MK <kaliszmat1@gmail.com>
2020-02-12 21:33:10 +0100

    automation testing - lessons

--- BASIC/app.js
+++ BASIC/app.js
@@ -7,4 +7,18 @@ app.get('/', (req,res)=>{
     res.send("WITAJ");
 });
 
+app.get('/error', (req,res)=>{
+    throw new Error('ayayay');
+});
+
+app.get('*', (req,res)=>{
+    res.status(404);
+    res.send('Not found');
+});
+
+app.use((err, req, res, next)=>{
+    res.status(500);
+    res.send('Error!');
+})
+
 exports.app = app;
--- BASIC/app.test.js
+++ BASIC/app.test.js
@@ -6,6 +6,24 @@ it('works', async ()=>{
     
     // expect(response).toMatchSnapshot();
     expect(response.status).toEqual(200);
-    expect(response.text).toEqual('Hello World');
+    expect(response.text).toEqual('WITAJ');
+
+});
+
+it('handles pages that are not found', async ()=>{
+    const response = await request(app).get('/whatever');
+
+    expect(response.status).toEqual(404);
+    expect(response.text).toEqual('Not found');
+    // expect(response.text).toMatchSnapshot();
+
+});
+
+it('handles pages that throw error', async ()=>{
+    const response = await request(app).get('/error');
+
+    expect(response.status).toEqual(500);
+    expect(response.text).toEqual('Error!');
+    // expect(response.text).toMatchSnapshot();
 
 });
